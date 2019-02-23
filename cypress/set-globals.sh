7ed1a11dc973f8f53389dcea6b0331cc4a42cb76
Steve Persch <steve.persch@pantheon.io>
2019-02-22 21:45:25 -0600

    set vars

--- cypress/set-globals.sh
+++ cypress/set-globals.sh
@@ -2,7 +2,7 @@
 
 set -eo pipefail
 (
-  echo 'export TERMINUS_ENV="pr-${PR_NUMBER}"'
+  echo 'export TERMINUS_ENV="pr-${CIRCLE_PULL_REQUEST##*/}"'
   echo 'export CYPRESS_baseUrl="https://${TERMINUS_ENV}-${TERMINUS_SITE}.pantheonsite.io/"'
 ) >> $BASH_ENV
 source $BASH_ENV

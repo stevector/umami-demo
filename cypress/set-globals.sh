761751cc5e2700f704460b0d73cfbb9efa31f797
Steve Persch <stevector@users.noreply.github.com>
2019-02-22 22:09:47 -0600

    Update set-globals.sh

--- cypress/set-globals.sh
+++ cypress/set-globals.sh
@@ -2,7 +2,6 @@
 
 set -eo pipefail
 (
-  echo 'export TERMINUS_ENV="pr-${CIRCLE_PULL_REQUEST##*/}"'
-  echo 'export CYPRESS_baseUrl="https://${TERMINUS_ENV}-${TERMINUS_SITE}.pantheonsite.io/"'
+  echo 'export CYPRESS_baseUrl="https://pr-${CIRCLE_PULL_REQUEST##*/}-${TERMINUS_SITE}.pantheonsite.io/"'
 ) >> $BASH_ENV
 source $BASH_ENV

5fcc0955fcb0b9d85425a9e2899ca2d68e74d310
Steve Persch <stevector@users.noreply.github.com>
2019-02-23 21:07:56 -0600

    whitespace

--- cypress/integration/umami/content-creation.spec.js
+++ cypress/integration/umami/content-creation.spec.js
@@ -1,6 +1,7 @@
 describe('Hello world ', function(){
   context('Make some nodes', function(){
     it('Logs in', function(){
+
       cy.visit('user');
       cy.get('input[name=name]').type(Cypress.env('DRUPAL_USERNAME'))
       cy.get('input[name=pass]').type(Cypress.env('DRUPAL_USERPASS') + '{enter}')

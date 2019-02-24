15ce616ca5f7af0b3f8d0a56c00c774ff59a39f2
Steve Persch <stevector@users.noreply.github.com>
2019-02-23 19:42:09 -0600

    Delete setup.sh

--- tests/cypress/setup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-terminus drush $TERMINUS_SITE.$TERMINUS_ENV -- user-create  $CYPRESS_DRUPAL_USERNAME  --password=$CYPRESS_DRUPAL_USERPASS
-terminus drush $TERMINUS_SITE.$TERMINUS_ENV -- user-add-role  administrator  $CYPRESS_DRUPAL_USERNAME

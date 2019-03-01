bd622c8162a5936350c6dd9484ba713d382826c1
Steve Persch <steve.persch@pantheon.io>
2019-02-28 21:45:30 -0600

    failover debugging

--- cypress/integration/umami/content-creation.spec.js
+++ cypress/integration/umami/content-creation.spec.js
@@ -5,13 +5,14 @@ describe('Hello world ', function(){
       cy.get('input[name=name]').type(Cypress.env('DRUPAL_USERNAME'))
       cy.get('input[name=pass]').type(Cypress.env('DRUPAL_USERPASS') + '{enter}')
 
-      cy.visit('node/add/article');
+      cy.visit('node/add/article?failover_debug=1');
       var d = new Date();
       var n = d.getTime();
       const title = 'Hello World ' + n;
       cy.get('input[name="title[0][value]"]').type(title, {force: true})
       cy.get('#edit-submit').click();
       cy.wait(500);
+
       cy.get('h1.page-title').contains(title);
     })
   })
